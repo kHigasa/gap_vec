7c89329cd2c506ba7d4b66c3cda500fbc295a0d7
khigasa <kojihigasa@gmail.com>
2019-03-01 00:02:32 +0900

    Impl insert remove

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-//! A vector which has gap inside the buf implemented with a growable
-//! ring buffer. It's written `GapVec<T>` but pronounced 'gap vector'.
+//! A contiguous growable array type with heap-allocated contens and gap. 
+//! It's written `GapVec<T>` but pronounced 'gap vector'.
 //!
 //! # Examples
 //!
@@ -27,9 +27,10 @@ use core::slice;
 
 use alloc::raw_vec::RawVec;
 use std::ops::Range;
+use std::ptr;
 
-/// A vector which has gap inside the buf implemented with a growable
-/// ring buffer. It's written `GapVec<T>` but pronounced 'gap vector'.
+/// A contiguous growable array type with heap-allocated contens and gap. 
+/// It's written `GapVec<T>` but pronounced 'gap vector'.
 ///
 /// # Examples
 ///
@@ -86,11 +87,11 @@ impl<T> GapVec<T> {
     ///
     /// // These are all done without reallocating
     /// for i in 0..10 {
-    ///     gap_vec.push(i);
+    ///     gap_vec.insert(i);
     /// }
     ///
     /// // ,but this may make the gap vector reallocate.
-    /// gap_vec.push(11);
+    /// gap_vec.insert(10);
     /// ```
     #[inline]
     pub fn with_capacity(capacity: usize) -> GapVec<T> {
@@ -138,14 +139,163 @@ impl<T> GapVec<T> {
     /// let gap_vec: GapVec<i32> = GapVec::new();
     /// assert_eq!(gap_vec.position(), 0);
     /// ```
+    #[inline]
     pub fn position(&self) -> usize {
         self.gap.start
+    }
+
+    /// Returns a reference to the `index`'th element,
+    /// or `None` if `index` is out of bounds.
+    pub fn get(&self, index: usize) -> Option<&T> {
+        let raw = self.index_to_raw(index);
+        if raw < self.capacity() {
+            unsafe {
+                Some(&*self.space(raw))
+            }
+        } else {
+            None
+        }
+    }
+
+    /// Sets the current insertion position to `pos`.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `pos > len`.
+    pub fn set_position(&mut self, pos: usize) {
+        if pos > self.len() {
+            panic!("index {} out of range for GapVec buffer", pos);
+        }
+
+        unsafe {
+            let gap = self.gap.clone();
+            if pos > gap.start {
+                let distance = pos - gap.start;
+                ptr::copy(self.space(pos), self.space_mut(gap.start), distance);
+            } else if pos < gap.start {
+                let distance = gap.start - pos;
+                ptr::copy(self.space(pos), self.space_mut(gap.end - distance), distance);
+            }
+
+            self.gap = pos .. pos + gap.len();
+        }
+    }
+
+    /// Inserts an element at gap start within the gap vector.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut gap_vec: GapVec<i32> = GapVec::new();
+    /// gap_vec.insert(3);
+    /// assert_eq!(gap_vec, [3]);
+    /// ```
+    pub fn insert(&mut self, element: T) {
+        if self.gap.len() == 0 {
+            self.enlarge_gap();
+        }
+
+        unsafe {
+            let index = self.gap.start;
+            ptr::write(self.space_mut(index), element);
+        }
+
+        self.gap.start += 1;
+    }
+
+    /// Removes and returns the element at gap end within the gap vector,
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut gap_vec: GapVec<i32> = GapVec::new();
+    /// gap_vec.insert(3);
+    /// gap_vec.set_position(0);
+    /// assert_eq!(gap_vec.remove(), 3);
+    /// ```
+    pub fn remove(&mut self) -> Option<T> {
+        if self.gap.end == self.capacity() {
+            return None;
+        }
+
+        let element = unsafe {
+            ptr::read(self.space(self.gap.end))
+        };
+        self.gap.end += 1;
+        Some(element)
+    }
+
+    // Returns the offset in the buffer of the `index`'th element, taking
+    // the gap into account. This does not check whether index is in range,
+    // but it never returns the index of space in the gap.
+    fn index_to_raw(&self, index: usize) -> usize {
+        if index < self.gap.start {
+            index
+        } else {
+            index + self.gap.len()
+        }
+    }
+
+    // Doubles the capacity of `self.buf`.
+    fn enlarge_gap(&mut self) {
+        let mut new_capacity = self.capacity() * 2;
+
+        if new_capacity == 0 {
+            new_capacity = 4;
+        }
+
+        let mut new_buf = RawVec::with_capacity(new_capacity);
+        let after_gap = self.capacity() - self.gap.end;
+        let new_gap = self.gap.start .. new_buf.cap() - after_gap;
+
+        unsafe {
+            // Copy buf before gap from self to new.
+            ptr::copy_nonoverlapping(self.space(0),
+                                     new_buf.ptr(),
+                                     self.gap.start);
+            // Copy buf after gap from self to new.
+            ptr::copy_nonoverlapping(self.space(self.gap.end),
+                                     new_buf.ptr().offset(new_gap.end as isize),
+                                     after_gap);
+        }
+
+        self.buf = new_buf;
+        self.gap = new_gap;
+    }
+
+    // Returns a pointer to the `index`'th element of the underlying buf,
+    // as if the gap were not there.
+    //
+    // Safety: `index` must be less than self.capacity().
+    unsafe fn space(&self, index: usize) -> *const T {
+        self.as_ptr().offset(index as isize)
+    }
+
+    // Returns a mutable pointer to the `index`'th element of the underlying buf,
+    // as if the gap were not there.
+    //
+    // Safety: `index` must be less than self.capacity().
+    unsafe fn space_mut(&mut self, index: usize) -> *mut T {
+        self.as_mut_ptr().offset(index as isize)
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Common trait implementations for Vec
 ////////////////////////////////////////////////////////////////////////////////
+
+impl<T> Drop for GapVec<T> {
+    fn drop(&mut self) {
+        unsafe {
+            for i in 0 .. self.gap.start {
+                ptr::drop_in_place(self.space_mut(i));
+            }
+            for i in self.gap.end .. self.capacity() {
+                ptr::drop_in_place(self.space_mut(i));
+            }
+        }
+    }
+}
 
 impl<T> Deref for GapVec<T> {
     type Target = [T];
